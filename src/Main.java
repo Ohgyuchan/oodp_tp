fd9a676c08d765f2c6a27a1a2d596bee1285a7b1
Ohgyuchan <gyu3637@gmail.com>
2022-05-12 19:09:56 +0900

    json read

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            Object ob = new JSONParser().parse(new FileReader("src/assets/data/users_data.json"));
+
+            JSONObject js = (JSONObject) ob;
+
+            System.out.println(js.get("users"));
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
     
     }
 
--- src/classes/auth/Login.java
+++ src/classes/auth/Login.java
@@ -1,61 +1,66 @@
-package classes.auth;
+// package classes.auth;
 
-import java.io.*;
-import java.util.*;
+// import java.io.*;
+// import java.util.*;
+// import org.json.simple.JSONObject;
+// import org.json.simple.parser.JSONParser;
+// import org.json.simple.parser.ParseException;
 
-import classes.Member;
-import classes.User;
+// import classes.Member;
+// import classes.User;
 
-public class Login {
+// public class Login {
 	
-	boolean login() {
-		String filename ="accs/account.text";
-		ArrayList<Member> account = new ArrayList<Member>();
-		Scanner sc = new Scanner(System.in);
-		
-		String uid;
-		String upw;
+// 	boolean login() {
+// 		Object ob = new JSONParser().parse(new FileReader("../../asset/data/users_data.json"));
 
-		System.out.println("id�� �Է��ϼ���:");
-		uid=sc.next();
-		System.out.println("pw�� �Է��ϼ���:");		
-		upw=sc.next();
+//         JSONObject js = (JSONObject) ob;
+// 		ArrayList<Member> account = new ArrayList<Member>();
+// 		Scanner sc = new Scanner(System.in);
 		
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(filename));
+// 		String uid;
+// 		String upw;
+
+// 		System.out.print("Input your ID: ");
+// 		uid=sc.next();
+// 		System.out.print("Input your Password: ");		
+// 		upw=sc.next();
+		
+// 		try {
+// 			BufferedReader br = new BufferedReader(new FileReader(filename));
 			
-			String sen;
-			String id, pw, name;
+// 			String sen;
+// 			String id, pw, name;
 			
-			while((sen=br.readLine())!=null) {
-				User inM = new User();
-				String[] temp = sen.split("/");
-				id =temp[0];
-				pw =temp[1];
-				name=temp[2];
-				inM.setId(id);
-				inM.setPassword(pw);
-				inM.setName(name);
-				account.add(inM);
-			}
-				br.close();
+// 			while((sen=br.readLine())!=null) {
+// 				User inM = new User();
+// 				String[] temp = sen.split("/");
+// 				id =temp[0];
+// 				pw =temp[1];
+// 				name=temp[2];
+// 				inM.setId(id);
+// 				inM.setPassword(pw);
+// 				inM.setName(name);
+// 				account.add(inM);
+// 			}
+// 				br.close();
 			
-			for(Member mem : account ) {
-				if(mem.getId().equals(uid)) {
-					if(mem.getPw().equals(upw)) {
-						System.out.println("�α��μ���");
-						System.out.println(mem.getName()+"�� �α����� ȯ���մϴ�");
-						return true;
-					}
-				}
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-}
+// 			for(Member mem : account ) {
+// 				if(mem.getId().equals(uid)) {
+// 					if(mem.getPw().equals(upw)) {
+// 						System.out.println("�α��μ���");
+// 						System.out.println(mem.getName()+"�� �α����� ȯ���մϴ�");
+// 						return true;
+// 					}
+// 				}
+// 			}
+// 		} catch (FileNotFoundException e) {
+// 			// TODO Auto-generated catch block
+// 			e.printStackTrace();
+// 		} catch (IOException e) {
+// 			// TODO Auto-generated catch block
+// 			e.printStackTrace();
+// 		}
+// 		return false;
+// 	}
+// }
