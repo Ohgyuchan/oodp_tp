c3bf253da818dddd6234e2e79917e7cdc9ef5923
Ohgyuchan <gyu3637@gmail.com>
2022-05-14 23:56:25 +0900

    singleton design getter & setter

--- src/classes/SingletonJSON.java
+++ src/classes/SingletonJSON.java
@@ -1,16 +1,69 @@
 package classes;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class SingletonJSON {
-    private static SingletonJSON instance = new SingletonJSON ();
-
+    private static SingletonJSON instance = new SingletonJSON();
+    private JSONObject usersJson;
+    private JSONObject projectsJson;
+    private JSONArray usersJsonArray;
+    private JSONArray projectsJsonArray;
 
     private SingletonJSON() {
+        this.setUsersJson();
+    }
+
+    public static SingletonJSON getInstance() {
+        return instance;
+    }
+
+    private void setUsersJson() {
+        Object usersData;
+        Object projectsData;
+        try {
+            usersData = new JSONParser().parse(new FileReader("src/assets/data/users_data.json"));
+            this.usersJson = (JSONObject) usersData;
+            this.usersJsonArray = (JSONArray) this.usersJson.get("users");
+
+            projectsData = new JSONParser().parse(new FileReader("src/assets/data/projects_data.json"));
+            this.projectsJson = (JSONObject) projectsData;
+            this.projectsJsonArray = (JSONArray) this.projectsJson.get("projects");
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public static JSONObject getInstance() {
-            return instance;
+    public JSONObject getUsersJson() {
+        return this.usersJson;
     }
+
+    public JSONObject getProjectsJson() {
+        return this.projectsJson;
+    }
+
+    public JSONArray getUsersJsonArray() {
+        return this.usersJsonArray;
+    }
+
+    public JSONArray getPrJsonArray() {
+        return this.projectsJsonArray;
+    }
+
+    public Project getProjectFromJson() {
+        return new Project();
+
+    }
+
+    public User getUserFromJSON() {
+        return new User();
+    }
+
 }
